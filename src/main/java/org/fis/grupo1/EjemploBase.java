fe85bc881dd40097f3b19a00d4b433f83902ff74
Juan Pablo Arias Buitrago <126124282+JuanParias29@users.noreply.github.com>
2025-03-02 15:55:46 -0500

    Update EjemploBase.java

--- src/main/java/org/fis/grupo1/EjemploBase.java
+++ src/main/java/org/fis/grupo1/EjemploBase.java
@@ -2,7 +2,7 @@ package org.fis;
 
 public class EjemploBase {
     // Este archivo es solo un ejemplo. No lo modifiques.
-    // Coloca tu código en tu propia carpeta dentro de /fis.
+    // Coloca tu código en un nuevo archivo java en tu propia carpeta de grupo dentro de /fis.
 
     public static void mostrarMensaje() {
         System.out.println("Este es un archivo de ejemplo para la estructura del proyecto.");
