e4e68c12f64af87ce25519cf479cde8f685d2362
Juan Pablo Arias Buitrago <126124282+JuanParias29@users.noreply.github.com>
2025-03-02 16:19:55 -0500

    🎨 Update Main.java

--- src/main/java/org/fis/Main.java
+++ src/main/java/org/fis/Main.java
@@ -3,7 +3,7 @@ package org.fis;
 public class Main { // main principal del proyecto (NO MODIFICAR)
     public static void main(String[] args) {
    
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hi and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             
