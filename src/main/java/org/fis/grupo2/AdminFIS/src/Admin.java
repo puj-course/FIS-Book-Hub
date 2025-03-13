e41d764d375d8ac8815c3545429a3b832b473833
Nicolas Diaz Granados <nicolasdiazgranados@javeriana.edu.co>
2025-03-13 10:44:45 -0500

    feat(Admin): Added cantidadLibros and analizarDatos Added main functionalities for Admin class Closes #9 Closes #4

---
+++ src/main/java/org/fis/grupo2/AdminFIS/src/Admin.java
@@ -0,0 +1,18 @@
+public class Admin {
+
+    int idAdmin;
+
+    public Admin (int idAdmin) {
+        this.idAdmin= idAdmin;
+    }
+
+    private void analizarDatos() {
+        //Realiza el proceso de analisis de datos del sistema para el administrador actual
+    }
+
+    private int cantidadLibrosDisponibles() {
+        int numLibros = 0;
+        //Devuelve la cantidad de libros disponibles en el sistema
+        return numLibros;
+    }
+}
