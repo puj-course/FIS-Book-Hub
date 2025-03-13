5a675f3b6dac3fe649a6de68a88803b8bf03159a
jorgegz10 <129014853+jorgegz10@users.noreply.github.com>
2025-03-13 18:51:38 -0500

    feat: ✨ Creación clase Bibliotecario #46

--- src/main/java/org/fis/grupo6/Bibliotecario.java
+++ src/main/java/org/fis/grupo6/Bibliotecario.java
@@ -4,10 +4,14 @@ public class Bibliotecario {
     public int idBibliotecario;
 
     public void registrarLibro(Libro libro) {
-        System.out.println("Libro " + libro.titulo + " registrado");
+        System.out.println("Libro  " + libro.titulo + " registrado");
     }
 
     public void registrarDevolucion(Prestamo prestamo) {
-        System.out.println("Devolución registrada");
+        System.out.println("Devolución registrada  ");
+    }
+
+    public void verificarRetrasos() {
+        System.out.println("No tiene retrasos");
     }
 }
