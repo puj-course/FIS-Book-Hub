0232b7b8e41bfbbe2fcf5aaa02ba82cb370011ff
Eliqn <eliqn.dev@hotmail.com>
2025-03-13 10:54:17 -0500

    feat: agregar funcionalidad de devolución #51

---
+++ src/main/java/org/fis/grupo6/Bibliotecario.java
@@ -0,0 +1,13 @@
+package org.fis.grupo6;
+
+public class Bibliotecario {
+    public int idBibliotecario;
+
+    public void registrarLibro(Libro libro) {
+        System.out.println("Libro " + libro.titulo + " registrado");
+    }
+
+    public void registrarDevolucion(Prestamo prestamo) {
+        System.out.println("Devolución registrada");
+    }
+}
