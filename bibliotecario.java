617a3ec10cdc73376a4ac7a7ce237ab49936fa1d
sara-munoz-5 <munozsara@javeriana.edu.co>
2025-03-13 10:54:43 -0500

    feat: clase bibliotecario #58 #48

---
+++ bibliotecario.java
@@ -0,0 +1,24 @@
+package prueba;
+extends administrador; 
+public class bibliotecario {
+	private int idBibliotecario;
+
+    public void Bibliotecario(int idBibliotecario) {
+        this.idBibliotecario = idBibliotecario;
+    }
+
+    public void registrarLibro(Libro libro) {
+        
+    }
+
+    public void verificarRetrasos() {
+       
+    }
+
+    public void registrarDevolucion(Prestamo prestamo) {
+   
+    }
+
+   
+
+}
