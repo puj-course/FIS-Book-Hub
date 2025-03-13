6f60b22085a19bee64e3779e12523ab691bbe87d
AndreMD287 <bastianlanfor@gmail.com>
2025-03-13 12:12:05 -0500

    feat: implementación de ver prestamos activos en clase Administrador #53

---
+++ Administrador-andre2.java
@@ -0,0 +1,9 @@
+package org.fis.grupo6;
+
+public class Administrador {
+    public int idAdministrador;
+    
+    public void cantidadPrestamosActivos() {
+        System.out.println("15 préstamos activos");
+    }
+}
