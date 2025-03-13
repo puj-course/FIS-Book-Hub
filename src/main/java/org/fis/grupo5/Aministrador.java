238aee7c90d3641d7e0024cf8bdf40e334172561
bello-juan <belloj-u@javeriana.edu.co>
2025-03-13 10:51:59 -0500

    feat: creacion del objeto Administrador con todos sus metodos implementados #47 #38 #54 :sparkle:

--- src/main/java/org/fis/grupo5/Aministrador.java
+++ src/main/java/org/fis/grupo5/Aministrador.java
@@ -5,16 +5,16 @@ public class Administrador {
     // Coloca tu código en un nuevo archivo java en tu propia carpeta de grupo dentro de /fis.
     private int idAdministrador;
     
-    /*public void analizarDatos() {                //analiza los datos del sistemas
-    }*/
+    public void analizarDatos() {                //analiza los datos del sistemas
+    }
     public void cantidadLibrosDisponibles() {            //devuelve un entero de la cantidad de libros disponibles para prestar
     }
-    /*public int cantidadPrestamosActivos() {              //devuelve un entero de la cantidad de prestamos activos   
+    public int cantidadPrestamosActivos() {              //devuelve un entero de la cantidad de prestamos activos   
     }
     public int cantidadLibrosDevueltosUltimoMes() {        //devuelve un entero de la cantidad de libros devueltos el ultimo mes
     }
     public list<String> listarUsuariosConRetrasios() {    //devuelve una lista de los usuarios que estan enretraso
-    }*/
+    }
 
     public static void main(String[] args) {
         mostrarMensaje();
