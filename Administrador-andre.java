9bc86164467b280773c58e4a7c74942522cc19a7
AndreMD287 <bastianlanfor@gmail.com>
2025-03-13 10:52:39 -0500

    feat: Implementación de ver libros disponibles en clase administrador #35

---
+++ Administrador-andre.java
@@ -0,0 +1,9 @@
+package org.fis.grupo6;
+
+public class Administrador {
+    public int idAdministrador;
+
+    public void cantidadLibrosDisponibles() {
+        System.out.println("100 libros disponibles");
+    }
+}
