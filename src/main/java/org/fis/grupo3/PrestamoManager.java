2c1e29d1ffb9feaf468a247a8befc062bf82fc00
Iván Pardo <123502604+Dimich1tri@users.noreply.github.com>
2025-05-15 10:54:43 -0500

    feat: nueva clase PrestamoManager para aplicacion patron mediator

---
+++ src/main/java/org/fis/grupo3/PrestamoManager.java
@@ -0,0 +1,15 @@
+public class PrestamoManager {
+    private static int contadorPrestamos = 1;
+
+    public static void procesarNuevoPrestamo(PrestamoMediator mediator) {
+        int id = contadorPrestamos++;
+        Prestamo prestamo = mediator.generarPrestamo(id);
+
+        if (prestamo != null) {
+            SistemaBiblioteca.getInstancia().agregarPrestamo(prestamo);
+            System.out.println("Préstamo registrado correctamente con ID: " + id);
+        } else {
+            System.out.println("No se pudo registrar el préstamo.");
+        }
+    }
+}
