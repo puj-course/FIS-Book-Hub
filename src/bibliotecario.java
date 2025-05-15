4c8c615d47018f780c00f45d4f9a0e72051d5db4
Santiago Galindo <galindosantiago@javeriana.edu.co>
2025-05-15 10:25:04 -0500

    🎨 #108 Actualización de la clase Bibliotecario

--- src/bibliotecario.java
+++ src/bibliotecario.java
@@ -1,24 +1,33 @@
-package prueba;
-extends administrador; 
-public class bibliotecario {
-	private int idBibliotecario;
+// ===================
+// CLASE BIBLIOTECARIO
+// ===================
+class Bibliotecario {
+    PrestamoFactory prestamoFactory;
+    Notificador notificador;
 
-    public void Bibliotecario(int idBibliotecario) {
-        this.idBibliotecario = idBibliotecario;
+    public Bibliotecario(PrestamoFactory factory, Notificador notificador) {
+        this.prestamoFactory = factory;
+        this.notificador = notificador;
     }
 
-    public void registrarLibro(Libro libro) {
-       libro.registrarLibro();   
+    public Prestamo registrarPrestamo(Usuario usuario, Libro libro) {
+        if (libro.numeroCopiasDisponibles <= 0) {
+            System.out.println("No hay copias disponibles");
+            return null;
+        }
+        Prestamo prestamo = prestamoFactory.crearPrestamo(usuario, libro);
+        libro.actualizarCopiasDisponibles(-1);
+        notificador.enviarMensaje("Préstamo registrado para " + usuario.nombre);
+        return prestamo;
     }
 
-    public void verificarRetrasos() {
-       prestamo.verificarRetraso();
+    public void registrarDevolucion(Prestamo prestamo, Libro libro) {
+        libro.actualizarCopiasDisponibles(1);
+        prestamo.estado = "Devuelto";
+        if (prestamo.verificarRetraso()) {
+            notificador.enviarMensaje("Usuario " + prestamo.usuario.nombre + " devolvió con retraso.");
+        } else {
+            notificador.enviarMensaje("Devolución exitosa de " + prestamo.usuario.nombre);
+        }
     }
-
-    public void registrarDevolucion(Prestamo prestamo) {
-        prestamo.registrarDevolucion();
-    }
-
-   
-
 }
