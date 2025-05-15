d6b8ba2c709530d1c92911fa13b29c1686920b3c
Laura-Ap37 <lsofia_aponte@javeriana.edu.co>
2025-05-15 10:24:55 -0500

    #108 🎨 actualizacion codigo usuario
    se actualiza el codigo de usuario

--- src/main/java/org/fis/grupo5/Usuario.java
+++ src/main/java/org/fis/grupo5/Usuario.java
@@ -1,16 +1,13 @@
-package org.fis;
+class Usuario {
+    String nombre;
+    PoliticaRetraso politicaRetraso;
 
-public class Usuario {
-    // Este archivo es solo un ejemplo. No lo modifiques.
-    // Coloca tu código en un nuevo archivo java en tu propia carpeta de grupo dentro de /fis.
-    private String nombre;
-    private String apellido;
-    private int cc;
-    private String correo;
-    private int telefono;
-    private String tipo;
-    
-    public  solicitarPrestamo(Libro libro) {                //metodo para solicitar prestamo de un libro especificado
+    public Usuario(String nombre, PoliticaRetraso politica) {
+        this.nombre = nombre;
+        this.politicaRetraso = politica;
     }
-    
+
+    public boolean tieneRetraso(Date fechaPrestamo, Date fechaDevolucionEstimada) {
+        return politicaRetraso.tieneRetraso(fechaPrestamo, fechaDevolucionEstimada);
+    }
 }
